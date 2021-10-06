b5712477ce46461416f21dad68d701b18c6fe783
Cannigia Laluw <cannigialaluw@gmail.com>
2021-10-05 23:48:29 -0400

    Initial structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { CssBaseline } from "@material-ui/core";
+
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 function App() {
   return <div>Beginning of Travenda</div>;
