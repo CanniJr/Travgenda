656b51343fc6a5eac089dfddce2ecb60a0b43bc0
Cannigia Laluw <cannigialaluw@gmail.com>
2021-10-06 23:54:55 -0400

    Base structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline, Grid } from "@material-ui/core";
 
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
 
 function App() {
-  return <div>Beginning of Travenda</div>;
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
