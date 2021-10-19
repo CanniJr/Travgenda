3cd3bf19ed1c90c0b883dc8a8097fd8c3323c286
Cannigia Laluw <cannigialaluw@gmail.com>
2021-10-18 23:59:04 -0400

    Add material UI ttoolbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,14 @@ const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography></Typography>
+        <Typography variant="h5" className={classes.title}>
+          Travgenda
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Options
+          </Typography>
+        </Box>
       </Toolbar>
     </AppBar>
   );
