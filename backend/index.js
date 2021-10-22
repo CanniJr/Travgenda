2aa8b1bb17bed2b8b2ac244234ca12dafe5fc70b
Cannigia Laluw <cannigialaluw@gmail.com>
2021-10-21 23:14:33 -0400

    Added models for users and pin

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB Connected!");
+  })
+  .catch((err) => console.log(err));
 
 app.listen(8800, () => {
   console.log("Server running...");
