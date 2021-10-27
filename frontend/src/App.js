cf88b0e01148d39afd26d3fca0818afe24f25bfb
Cannigia Laluw <cannigialaluw@gmail.com>
2021-10-26 23:52:44 -0400

    added mapbox dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
+
 function App() {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    logitude: -122.4376,
+    zoom: 8,
+  });
+
   return <div className="App">Travgenda</div>;
 }
 
