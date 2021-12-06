a2985b85ab4bbade317847c33cb89cd59fa9f33c
Cannigia Laluw <cannigialaluw@gmail.com>
2021-12-05 19:53:17 -0500

    adding registration cancellation

--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./register.css";
 import PlaceIcon from "@mui/icons-material/Place";
+import axios from "axios";
 
 function Register() {
+  const [success, setSuccess] = useState(false);
+  const [fail, setFail] = useState(false);
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newUser = {
+      username: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passRef.current.value,
+    };
+
+    try {
+      await axios.post("/users/register", newUser);
+      setFail(false);
+    } catch (error) {
+      setFail(true);
+    }
+  };
+
   return (
     <div className="register__container">
       <div className="logo">
@@ -10,12 +33,19 @@ function Register() {
         Travgenda
       </div>
 
-      <form>
-        <input type="text" placeholder="username" />
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <button>Register</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="username" ref={nameRef} />
+        <input type="email" placeholder="email" ref={emailRef} />
+        <input type="password" placeholder="password" ref={passRef} />
+        <button className="register__button">Register</button>
+        {success && (
+          <span className="success">
+            Welcome to Travgenda! You can now login.
+          </span>
+        )}
+        {fail && <span className="fail">Please try again!</span>}
       </form>
+      {/* Add registration cancelation here! */}
     </div>
   );
 }
