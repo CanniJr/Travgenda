6f65804db3d99a6d897be5d04752f3c3d040c859
Cannigia Laluw <cannigialaluw@gmail.com>
2021-12-11 02:59:17 -0500

    fixes for deploy

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use("/pins", pinRoute);
 app.use("/users", userRoute);
 
 // Heroku build server request
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "/frontend")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
 });
