f9840fec3196657999c4ad0fbfb834882b1d0bc6
Cannigia Laluw <cannigialaluw@gmail.com>
2023-08-14 15:08:51 -0700

    moved frontend to root and replaced bcrypt with bcryptjs

--- backend/index.js
+++ backend/index.js
@@ -21,10 +21,10 @@ app.use("/pins", pinRoute);
 app.use("/users", userRoute);
 
 // Heroku build server request
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
-});
+// app.use(express.static(path.join(__dirname, "/frontend/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
+// });
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on ${process.env.PORT}...`);
